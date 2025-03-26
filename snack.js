aebd19f7159690c14d2186dd7dc2c41b0b84775c
nicolaparutto <nicolaparutto2003@gmail.com>
2025-03-26 16:56:35 +0100

    feat: first exercise + bonus completed

---
+++ snack.js
@@ -0,0 +1,45 @@
+/* 
+🏆 Crea una funzione getPostTitle(id) che accetta un id e restituisce una 
+Promise che recupera il titolo di un post dal link
+https://dummyjson.com/posts/{id}
+
+🎯 Bonus: Ottieni l'intero post con l'autore
+Crea una funzione getPost(id) che recupera l'intero post. Concatena una 
+seconda chiamata che aggiunge una proprietà user che contiene i dati 
+dell'autore, recuperati dalla 
+chiamata https://dummyjson.com/users/{post.userId}.
+*/
+//Esercizio base:
+function getPostTitle(id) {
+	return new Promise((resolve, reject) => {
+		fetch(`https://dummyjson.com/posts/${id}`)
+			.then(response => response.json())
+			.then(data => resolve(data.title))
+			.catch(error => reject(error))
+	})
+}
+
+// getPostTitle(2)
+// 	.then(response => console.log(response))
+// 	.catch(error => console.error(error));
+
+//Esercizio bonus:
+function getPost(postId) {
+	return new Promise((resolve, reject) => {
+		fetch(`https://dummyjson.com/posts/${postId}`)
+			.then(response => response.json())
+			.then(postData => {
+				fetch(`https://dummyjson.com/users/${postData.userId}`)
+					.then(response => response.json())
+					.then(userData => {
+						postData.user = userData
+						resolve(postData)
+					})
+			})
+			.catch(error => reject(error))
+	})
+}
+
+// getPost(2)
+// 	.then(response => console.log(response))
+// 	.catch(error => console.log(error))
